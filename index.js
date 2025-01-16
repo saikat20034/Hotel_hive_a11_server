931117c81b76a2f701801917101fa9a1ede543fc
saikat20034 <mazharulislamsaikat2000@gmail.com>
2025-01-16 19:29:06 +0600

    Token verify done

--- index.js
+++ index.js
@@ -96,12 +96,12 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/rooms/:id', verifyToken, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await HotelCollection.findOne(query);
-      res.send(result);
-    });
+    // app.get('/rooms/:id', verifyToken, async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await HotelCollection.findOne(query);
+    //   res.send(result);
+    // });
 
     app.put('/rooms/update/:id', async (req, res) => {
       const id = req.params.id;
