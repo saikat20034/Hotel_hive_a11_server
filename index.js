70189e146914bef4b5ad4a2f44d3d6e9fa6c71c7
saikat20034 <mazharulislamsaikat2000@gmail.com>
2025-01-16 19:32:43 +0600

    Cookie clear when logout

--- index.js
+++ index.js
@@ -63,19 +63,19 @@ async function run() {
       res.send(result);
     });
 
-    // app.post('/jwt', async (req, res) => {
-    //   const user = req.body;
-    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    //     expiresIn: '1d',
-    //   });
-    //   res
-    //     .cookie('token', token, {
-    //       httpOnly: true,
-    //       secure: process.env.NODE_ENV === 'production',
-    //       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    //     })
-    //     .send({ success: true });
-    // });
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d',
+      });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true });
+    });
 
     app.get('/logout', (req, res) => {
       res
